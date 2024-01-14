f0712286dd897b17173a45d5d34416b3698ef32c
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 12:17:56 -0300

    create flight use cases test

--- application/src/test/java/dev/grigolli/example/dddhexarchi/application/flight/create/CreateFlightUseCaseTest.java
+++ application/src/test/java/dev/grigolli/example/dddhexarchi/application/flight/create/CreateFlightUseCaseTest.java
@@ -180,7 +180,295 @@ class CreateFlightUseCaseTest extends UseCaseTest {
         final var expectedDepartureAirport = "GRU";
         final var expectedArrivalAirport = "CGR";
 
-        final var expectedErrorMessage = "Departure date is required";
+        final var expectedErrorMessage = "Scheduled departure time is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithNullScheduledArrivalTime_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final LocalTime expectedScheduledArrivalTime = null;
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "GRU";
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Scheduled arrival time is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithNullAircraftID_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final String expectedAircraftID = null;
+        final var expectedDepartureAirport = "GRU";
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Aircraft ID is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithEmptyAircraftID_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "";
+        final var expectedDepartureAirport = "GRU";
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Aircraft ID is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithEmptyDepartureAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "";
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Departure airport is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithNullDepartureAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final String expectedDepartureAirport = null;
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Departure airport is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithWrongCodeDepartureAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "ASDDS";
+        final var expectedArrivalAirport = "CGR";
+
+        final var expectedErrorMessage = "Airport code must be 3 characters";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithEmptyArrivalAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "GRU";
+        final var expectedArrivalAirport = "";
+
+        final var expectedErrorMessage = "Arrival airport is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithNullArrivalAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "GRU";
+        final String expectedArrivalAirport = null;
+
+        final var expectedErrorMessage = "Arrival airport is required";
+        final var expectedErrorCount = 1;
+        // when
+        final var aCommand = CreateFlightCommand.with(
+                expectedFlightNumber,
+                expectedDepartureDate,
+                expectedScheduledDepartureTime,
+                expectedScheduledArrivalTime,
+                expectedAircraftID,
+                expectedDepartureAirport,
+                expectedArrivalAirport);
+
+        // then
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Mockito.verify(flightGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAnInvalidCommandWithWrongCodeArrivalAirport_whenCallExecute_thenShouldThrowException() {
+        final var expectedFlightNumber = "JJ1234";
+        final var expectedDepartureDate = LocalDate.of(2024, 1, 12);
+        final var expectedScheduledDepartureTime = LocalTime.of(17, 20);
+        final var expectedScheduledArrivalTime = LocalTime.of(18, 10);
+        final var expectedAircraftID = "B737";
+        final var expectedDepartureAirport = "GRU";
+        final var expectedArrivalAirport = "ASDDS";
+
+        final var expectedErrorMessage = "Airport code must be 3 characters";
         final var expectedErrorCount = 1;
         // when
         final var aCommand = CreateFlightCommand.with(
