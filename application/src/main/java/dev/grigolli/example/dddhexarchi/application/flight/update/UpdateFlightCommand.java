6de2ddb21accc769d300b915f29befa72aeba96c
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 12:00:35 -0300

    update flight use case

---
+++ application/src/main/java/dev/grigolli/example/dddhexarchi/application/flight/update/UpdateFlightCommand.java
@@ -0,0 +1,15 @@
+package dev.grigolli.example.dddhexarchi.application.flight.update;
+
+public record UpdateFlightCommand(
+        //TODO: Attributes here
+) {
+
+    public static UpdateFlightCommand with(
+            //TODO: Attributes here
+    ) {
+        return new UpdateFlightCommand(
+                //TODO: Attributes here
+        );
+
+    }
+}
