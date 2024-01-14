533a5a526210b540049331289bc24ea7583a09b5
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 12:00:18 -0300

    flight gateway

---
+++ domain/src/main/java/dev/grigolli/example/dddhexarchi/domain/flight/FlightGateway.java
@@ -0,0 +1,6 @@
+package dev.grigolli.example.dddhexarchi.domain.flight;
+
+import dev.grigolli.example.dddhexarchi.domain.utils.DomainGateway;
+
+public interface FlightGateway extends DomainGateway<Flight, FlightID> {
+}
