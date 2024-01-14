38492a038f7c35adb8264b0206c810cf3cfcbe5a
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 12:21:41 -0300

    DefaultCreateFlightUseCase execute implementation

--- application/src/main/java/dev/grigolli/example/dddhexarchi/application/flight/create/DefaultCreateFlightUseCase.java
+++ application/src/main/java/dev/grigolli/example/dddhexarchi/application/flight/create/DefaultCreateFlightUseCase.java
@@ -1,6 +1,9 @@
 package dev.grigolli.example.dddhexarchi.application.flight.create;
 
+import dev.grigolli.example.dddhexarchi.domain.exceptions.NotificationException;
+import dev.grigolli.example.dddhexarchi.domain.flight.Flight;
 import dev.grigolli.example.dddhexarchi.domain.flight.FlightGateway;
+import dev.grigolli.example.dddhexarchi.domain.validation.handler.Notification;
 
 import java.util.Objects;
 
@@ -14,8 +17,22 @@ public class DefaultCreateFlightUseCase extends CreateFlightUseCase {
 
     @Override
     public CreateFlightOutput execute(CreateFlightCommand anInput) {
-        //TODO: implement method
-        return null;
+        final var flightNumber = anInput.flightNumber();
+        final var departureDate = anInput.departureDate();
+        final var scheduledDepartureTime = anInput.scheduledDepartureTime();
+        final var scheduledArrivalTime = anInput.scheduledArrivalTime();
+        final var aircraftID = anInput.aircraftID();
+        final var departureAirport = anInput.departureAirport();
+        final var arrivalAirport = anInput.arrivalAirport();
+
+        final var notification = Notification.create();
+        final var aFlight = notification.validate(() -> Flight.newFlight(flightNumber, departureDate, scheduledDepartureTime, scheduledArrivalTime, aircraftID, departureAirport, arrivalAirport));
+
+        if(notification.hasErrors()) {
+            throw new NotificationException("Could not create Aggregate Flight", notification);
+        }
+
+        return CreateFlightOutput.from(this.flightGateway.create(aFlight));
     }
 
 }
