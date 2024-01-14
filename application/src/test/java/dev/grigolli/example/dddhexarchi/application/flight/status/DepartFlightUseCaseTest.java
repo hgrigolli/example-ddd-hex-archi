9f87d5941bbb821e0c57dae00f8697a66fd3ee8e
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 14:09:16 -0300

    preparing status usecase tests with usecasetest file template

---
+++ application/src/test/java/dev/grigolli/example/dddhexarchi/application/flight/status/DepartFlightUseCaseTest.java
@@ -0,0 +1,26 @@
+package dev.grigolli.example.dddhexarchi.application.flight.status;
+
+import dev.grigolli.example.dddhexarchi.application.UseCaseTest;
+import dev.grigolli.example.dddhexarchi.domain.flight.FlightGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+class DepartFlightUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultDepartFlightUseCase useCase;
+
+    @Mock
+    private FlightGateway flightGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(flightGateway);
+    }
+
+}
