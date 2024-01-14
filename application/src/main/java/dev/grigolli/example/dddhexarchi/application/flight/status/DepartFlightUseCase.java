6e3bf96dca9ec23ac5cb349b45c61c981251564c
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 18:14:54 -0300

    DepartFlightUseCase test and implementation

---
+++ application/src/main/java/dev/grigolli/example/dddhexarchi/application/flight/status/DepartFlightUseCase.java
@@ -0,0 +1,6 @@
+package dev.grigolli.example.dddhexarchi.application.flight.status;
+
+import dev.grigolli.example.dddhexarchi.application.UnitUseCase;
+
+public abstract class DepartFlightUseCase extends UnitUseCase<String> {
+}
