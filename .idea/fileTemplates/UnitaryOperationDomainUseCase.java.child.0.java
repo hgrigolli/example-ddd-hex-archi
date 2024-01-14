5636284e682e4ed09f9a38f9b48a76dff2c952ee
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 15:10:43 -0300

    UnitaryOperationDomainUseCase file template

---
+++ .idea/fileTemplates/UnitaryOperationDomainUseCase.java.child.0.java
@@ -0,0 +1,19 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+#set($lowerName = $Domain.substring(0,1).toLowerCase() + $Domain.substring(1))
+
+public class Default${Operation}${Domain}UseCase extends ${Operation}${Domain}UseCase {
+
+    private final ${Domain}Gateway ${lowerName}Gateway;
+    
+    public Default${Operation}${Domain}UseCase(final ${Domain}Gateway a${Domain}Gateway) {
+        this.${lowerName}Gateway = Objects.requireNonNull(a${Domain}Gateway);
+    }
+    
+    @Override
+    public void execute(${InputType} a${InputType}) {
+        //TODO: implement execute method
+    }
+    
+}
