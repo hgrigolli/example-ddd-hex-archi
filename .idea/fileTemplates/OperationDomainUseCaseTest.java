7f25f0a976a5491e8f81e511cf838028dd866a87
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-14 12:29:01 -0300

    sonarlint suggestion

--- .idea/fileTemplates/OperationDomainUseCaseTest.java
+++ .idea/fileTemplates/OperationDomainUseCaseTest.java
@@ -11,7 +11,7 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
-public class ${Operation}${Domain}UseCaseTest extends UseCaseTest {
+class ${Operation}${Domain}UseCaseTest extends UseCaseTest {
 
     @InjectMocks
     private Default${Operation}${Domain}UseCase useCase;
