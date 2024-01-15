7887a2b42f733e46224d5c5520f958577e577351
hgrigolli <henrigrigo@gmail.com>
2024-01-15 14:48:36 -0300

    FlightPostgreSQLGateway implementation

--- infrastructure/src/main/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/FlightPostgreSQLGateway.java
+++ infrastructure/src/main/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/FlightPostgreSQLGateway.java
@@ -5,39 +5,97 @@ import dev.grigolli.example.dddhexarchi.domain.flight.FlightGateway;
 import dev.grigolli.example.dddhexarchi.domain.flight.FlightID;
 import dev.grigolli.example.dddhexarchi.domain.pagination.Pagination;
 import dev.grigolli.example.dddhexarchi.domain.pagination.SearchQuery;
+import dev.grigolli.example.dddhexarchi.infrastructure.flight.persistence.FlightJpaEntity;
+import dev.grigolli.example.dddhexarchi.infrastructure.flight.persistence.FlightRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
+import static dev.grigolli.example.dddhexarchi.infrastructure.utils.SpecificationUtils.like;
+
+@Component
 public class FlightPostgreSQLGateway implements FlightGateway {
 
+    private final FlightRepository flightRepository;
+
+    public FlightPostgreSQLGateway(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
     @Override
-    public Flight create(Flight aDomain) {
-        return null;
+    public Flight create(Flight aFlight) {
+        return save(aFlight);
     }
 
     @Override
     public void deleteById(FlightID anID) {
-
+        final String anIDValue = anID.getValue();
+        if(this.flightRepository.existsById(anIDValue)) {
+            this.flightRepository.deleteById(anIDValue);
+        }
     }
 
     @Override
     public Optional<Flight> findById(FlightID anID) {
-        return Optional.empty();
+        return this.flightRepository.findById(anID.getValue())
+                .map(FlightJpaEntity::toAggregate);
     }
 
     @Override
-    public Flight update(Flight aDomain) {
-        return null;
+    public Flight update(Flight aFlight) {
+        return save(aFlight);
     }
 
     @Override
     public Pagination<Flight> findAll(SearchQuery aQuery) {
-        return null;
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()),aQuery.sort())
+        );
+        //Busca dinamica pelo criterio terms
+        final var specifications = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map(this::assembleSpecification)
+                .orElse(null);
+
+        final var pageResult = this.flightRepository.findAll(Specification.where(specifications), page);
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(FlightJpaEntity::toAggregate).toList()
+        );
     }
 
     @Override
     public List<FlightID> existsByIds(Iterable<FlightID> flightIDS) {
-        return null;
+        final var ids = StreamSupport.stream(flightIDS.spliterator(), false)
+                .map(FlightID::getValue)
+                .toList();
+
+        return this.flightRepository.existsByIds(ids).stream()
+                .map(FlightID::from)
+                .toList();
+    }
+
+    private Flight save(Flight aFlight) {
+        return this.flightRepository.save(FlightJpaEntity.from(aFlight)).toAggregate();
+    }
+
+    private Specification<FlightJpaEntity> assembleSpecification(final String str) {
+        final Specification<FlightJpaEntity> flightNumberLike = like("flightNumber", str);
+        final Specification<FlightJpaEntity> departureAirportLike = like("departureAirport", str);
+        final Specification<FlightJpaEntity> arrivalAirportLike = like("arrivalAirport", str);
+        final Specification<FlightJpaEntity> flightStatusLike = like("flightStatus", str);
+        final Specification<FlightJpaEntity> aircraftIDLike = like("aircraftID", str);
+
+        return flightNumberLike.or(departureAirportLike).or(arrivalAirportLike).or(flightStatusLike).or(aircraftIDLike);
     }
 }
--- infrastructure/src/main/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/persistence/FlightRepository.java
+++ infrastructure/src/main/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/persistence/FlightRepository.java
@@ -1,6 +1,18 @@
 package dev.grigolli.example.dddhexarchi.infrastructure.flight.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FlightRepository extends JpaRepository<FlightJpaEntity, String> {
+
+    Page<FlightJpaEntity> findAll(Specification<FlightJpaEntity> whereClause, Pageable pageable);
+
+    @Query(value = "select f.id from Flight f where f.id in :ids")
+    List<String> existsByIds(@Param("ids") List<String> ids);
 }
