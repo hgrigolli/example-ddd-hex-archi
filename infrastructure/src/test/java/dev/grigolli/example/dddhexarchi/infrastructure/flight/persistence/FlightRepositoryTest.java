cd051fe181cd506cb4a9e75838881ae26f8b649d
Henrique Grigolli <henrigrigo@gmail.com>
2024-01-15 08:12:29 -0300

    preparing infrastructure module

--- infrastructure/src/test/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/persistence/FlightRepositoryTest.java
+++ infrastructure/src/test/java/dev/grigolli/example/dddhexarchi/infrastructure/flight/persistence/FlightRepositoryTest.java
@@ -1,4 +1,18 @@
 package dev.grigolli.example.dddhexarchi.infrastructure.flight.persistence;
 
-public class FlightRepositoryTest {
+import dev.grigolli.example.dddhexarchi.PostgreSQLGatewayTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@PostgreSQLGatewayTest
+class FlightRepositoryTest {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Test
+    void givenAValidFlight_whenSave_thenShouldPersist() {
+        
+    }
+
 }
